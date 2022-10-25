7d7f4fdb411a175cd9f78ca589c63e7bd6e427c8
zahidi14 <d.zahidi1412@gmail.com>
2022-10-25 11:59:48 +0800

    fix image

---
+++ script/script.js
@@ -0,0 +1,6 @@
+    var nav = document.querySelector(".listt");
+    const button = document.querySelector(".burger");
+
+    button.addEventListener("click", () =>{
+        nav.classList.toggle("showNav")
+    })
